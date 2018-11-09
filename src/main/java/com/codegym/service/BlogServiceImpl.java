630dbaa243c043a66b3bcb2559c7f000abe678a7
SON NGUYEN <myantj3m@gmail.com>
2018-11-09 18:47:12 +0700

    ahihi

---
+++ src/main/java/com/codegym/service/BlogServiceImpl.java
@@ -0,0 +1,32 @@
+package com.codegym.service;
+
+import com.codegym.model.Blog;
+import com.codegym.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BlogServiceImpl implements BlogService {
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @Override
+    public List<Blog> findAll() {
+        return blogRepository.findAll();
+    }
+
+    @Override
+    public Blog findById(int id) {
+        return blogRepository.findById(id);
+    }
+
+    @Override
+    public void save(Blog blog) {
+        blogRepository.save(blog);
+    }
+
+    @Override
+    public void remove(int id) {
+        blogRepository.remove(id);
+    }
+}
